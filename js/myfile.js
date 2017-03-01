20604bc30ed3519d970bc3220f204d25e29226ab
David Anderson <David Anderson>
2017-03-01 00:11:46 -0500

    add everything

---
+++ js/myfile.js
@@ -0,0 +1,9 @@
+
+(function($) {
+    "use strict"; // Start of use strict
+    
+    $('#collapseOne').on('show', function () {
+        window.location.href = 'resume/Resume.pdf';
+    })
+
+})(jQuery); // End of use strict
